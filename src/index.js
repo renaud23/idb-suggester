bf9e5f00b23a871d2df0c5ef5b46923ee30e4716
Renaud Genevois <KQHLZ2@ad.insee.intra>
2020-09-02 15:42:06 +0200

    force release

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import { StoreManager } from './store-manager';
 export { Suggester } from './suggester';
 export { StoreManager } from './store-manager';
 export { createStore } from './store-index';
+export { default as createWorker } from './worker/bulk-insert';
 
 export default { Suggester, StoreManager };
